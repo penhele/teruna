8c7aa16ec6b41a537b02254396070f0187b06919
penhele <stephenhelk@gmail.com>
2025-10-15 17:07:53 +0700

    change middleware

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,11 @@ import { auth } from "@/auth";
 
 const ProtectedRoutes = ["/dashboard"];
 
-export async function middleware(request: NextRequest) {
-  const session = await auth();
-  const isLoggedIn = !!session?.user;
-  const role = session?.user.role;
-  const { pathname } = request.nextUrl;
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("authjs.session-token"); // sesuaikan nama cookie next-auth
+
+  const isLoggedIn = !!token;
+  const pathname = request.nextUrl.pathname;
 
   if (
     !isLoggedIn &&
@@ -16,13 +16,11 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/signin", request.url));
   }
 
-  if (isLoggedIn && role !== "admin" && pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
   if (isLoggedIn && pathname.startsWith("/signin")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
